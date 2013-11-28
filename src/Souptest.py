007c8e6ceb4d30c3771932ec46b993afdd7abb70
Bob <bob.vankamp@gmail.com>
2013-11-28 20:06:09 +0100

    Edited souptest
    Souptest opent nu een pagina en geeft alle links die daar staan terug,
    perfect voor crawlen.

--- src/Souptest.py
+++ src/Souptest.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-c=urllib2.urlopen('http://gaanders.home.xs4all.nl/pages/cv.html')
+c=urllib2.urlopen('http://www.jobbird.com/nl/vacatures/124/techniek-industrie')
 soup = BeautifulSoup(c, "lxml")
-result = soup.find('div', {'id':'personaliarechts'})
-print result.find('p').text
+baseUrl = 'http://www.jobbird.com'
+for a in soup.findAll('a'):
+    if a.has_attr('href'):
+        ref = a['href']
+        if(ref.find('http') == 0):
+            print a['href']
+        elif(ref.find('/') == 0):
+            print baseUrl+ref
