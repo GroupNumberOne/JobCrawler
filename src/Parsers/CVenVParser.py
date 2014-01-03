2f58b1d011d4adc5545b9c58137244a7bfa234f6
Bob <bob.vankamp@gmail.com>
2014-01-03 23:47:59 +0100

    Parsers verbeterd
    We krijgen nu meer info

--- src/Parsers/CVenVParser.py
+++ src/Parsers/CVenVParser.py
@@ -9,6 +9,7 @@ import psycopg2
 from bs4 import BeautifulSoup
 import re
 import os,sys,inspect
+import urllib2
 
 from DbHandler import DbHandler
     
@@ -17,6 +18,7 @@ class CVenVParser:
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     parentdir = os.path.dirname(currentdir)
     sys.path.insert(0,parentdir)
+    kennisArray = ['apache','php','java','c++','c#','javascript','.net','html','css','python','ruby','perl','mysql','oracle','postgresql','android','vmware','istqb','tmap']
     
     db = DbHandler()
             
@@ -24,15 +26,42 @@ class CVenVParser:
         tag = soup.find(tag,text=re.compile('.*'+v+'.*'))
         return None if tag is None else tag.parent.find('div').text
     
+    def handleKennis(self,kennis):
+        kennis_string = ''
+        kennis = kennis.lower()
+        for k in self.kennisArray:
+            k=k.lower()
+            if kennis.find(k) > 0:
+                kennis_string += k+', '
+                
+        kennis_string = kennis_string.rstrip(', ')
+        return kennis_string
+                
+    def handleExperience(self,soup):
+        soup = str(soup.find('div', {'class':'detail'}).getText())
+        start = soup.find('ICT/ Automatisering')
+        end = soup.find(')',start)
+        ervaring = [int(s) for s in list(soup[start:end+1]) if s.isdigit()]
+
+        if len(ervaring) > 0:
+            ervaring = ervaring[0]
+        else:
+            ervaring = None
+            
+        return ervaring
+    
     def parseCV(self,soup,fullUrl=None):
         if self.findValues(soup,'ICT/ Automatisering','div') is None: #Means we have someone with ICT experience
             return
         
+        jaren_werkervaring = self.handleExperience(soup)
         beroep = self.findValues(soup,'Beroep')
         opleiding = self.findValues(soup,'Niveau')
         woonplaats = self.findValues(soup,'Woonplaats')
         provincie = self.findValues(soup,'Provincie')
         leeftijd = self.findValues(soup,'Leeftijd')
+        kennis = self.handleKennis(soup.find('div', {'class':'detail'}).getText())
+
         if leeftijd != None:
             leeftijd = leeftijd.split()[0]
         
@@ -41,7 +70,7 @@ class CVenVParser:
         else:
             rijbewijs = False    
         
-        cvData = {'beroep':beroep, 'opleiding': opleiding, 'woonplaats':woonplaats,'rijbewijs':rijbewijs}
+        cvData = {'beroep':beroep, 'opleiding': opleiding, 'woonplaats':woonplaats,'rijbewijs':rijbewijs, 'jaren_werkervaring':jaren_werkervaring,'it_kennis':kennis}
         
         self.db.insertCV(cvData,fullUrl)
         
@@ -53,7 +82,7 @@ class CVenVParser:
         opleiding = self.findValues(soup,'Niveau')
         dienstverband = self.findValues(soup,'Dienstverband')
         plaats = self.findValues(soup,'Regio')
-        kennis = self.findValues(soup,'Kennis')
+        kennis = self.handleKennis(self.findValues(soup,'Kennis'))
         omschrijving = self.findValues(soup,'Functieomschrijving')
         
         vacatureData = {'functie':beroep,'opleiding':opleiding,'dienstverband':dienstverband,'plaats':plaats,'it_kennis':kennis,'omschrijving':omschrijving}
--- src/Parsers/MBParser.py
+++ src/Parsers/MBParser.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 import re
 import os,sys,inspect
 import logging
+import urllib2
 
 from DbHandler import DbHandler
 
@@ -18,17 +19,30 @@ class MBParser:
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     parentdir = os.path.dirname(currentdir)
     sys.path.insert(0,parentdir)
+    
+    kennisArray = ['apache','php','uml','java','c++','c#','javascript','.net','html','css','python','ruby','perl','mysql','oracle','postgresql','android','vmware','istqb','tmap']
         
     db = DbHandler()    
         
     def findValues(self,soup,v,tag='span'):
         tag = soup.find(tag,text=re.compile('.*'+v+'.*'))
         return None if tag is None else tag.parent.find('div').text
+    
+    def handleKennis(self,kennis):
+        kennis_string = ''
+        kennis = kennis.lower()
+        for k in self.kennisArray:
+            k = k.lower()
+            if kennis.find(k) > 0:
+                kennis_string += k+', '
+                
+        kennis_string = kennis_string.rstrip(', ')
+        return kennis_string
         
     def parseVacature(self,soup,fullUrl=None):
         if soup.find('div', {'class':'additionalinformation'}) is None:
             return
-        print 'Parsing...'
+
         if soup.find('span', {'itemprop':'occupationalCategory'}).getText() == 'IT / Software Development' and soup.find('div', {'class':'additionalinformation'}) is not None:
             try:
                 beroep = soup.find('h1', {'class':'jobtitle'}).getText()
@@ -40,18 +54,25 @@ class MBParser:
                 opleiding = ''
             try:
                 ervaring = soup.find('span', {'itemprop':'experienceRequirements'}).getText()
+                ervaring = [int(s) for s in ervaring.split() if s.isdigit()]
+                ervaring = ervaring[0]
             except:
                 ervaring = ''
             try:
                 plaats = soup.find('div', {'class':'additionalinformation'}).find('span',{'class':'wrappable','itemprop':None}).getText()
             except:
                 plaats = ''
+            try:
+                it_kennis = self.handleKennis(soup.find('div',{'itemprop':'description'}).getText())
+            except:
+                it_kennis = ''
             
-            print 'Done parsing'
-        
-            logging.info("Inserted "+fullUrl)
-            
-            vacatureData = {'functie':beroep,'plaats':plaats,'opleiding':opleiding,'jaren_werkervaring':ervaring}
-            logging.info(vacatureData)
+            vacatureData = {'functie':beroep,'plaats':plaats,'opleiding':opleiding,'jaren_werkervaring':ervaring,'it_kennis':it_kennis}
+            #print vacatureData
             
             self.db.insertVacature(vacatureData, fullUrl)
+
+#feed = "http://vacature.monsterboard.nl/Backend-Java-Developer-Zakelijke-Dienstverlener-Vacature-Eindhoven-Noord-Brabant-Nederland-128775423.aspx"
+#c=urllib2.urlopen(feed)
+#soup = BeautifulSoup(c, 'html5lib')
+#MBParser().parseVacature(soup, feed)
